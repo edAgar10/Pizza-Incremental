d3e00815cce4b7013f8594f5018aff7109eaac1f
edAgar10 <edward10agar@gmail.com>
2024-09-10 17:09:02 +0100

    Add files via upload
    camera controls

--- main.js
+++ main.js
@@ -30,25 +30,78 @@ group.add(sphere)
 scene.add(group)
 
 camera.position.z = 20;
+camera.position.z.clamp
 
-const mouse = {
-	x: undefined,
-	y: undefined
+// const mouse = {
+	// x: undefined,
+	// y: undefined
+// }
+
+var mouseDown = false,
+	mouseX = 0,
+	mouseY = 0;
+	
+
+	
+function onMouseMove(evt) {
+	if(!mouseDown) {
+		return
+	}
+	
+	evt.preventDefault();
+	
+	var deltaX = evt.clientX - mouseX;
+	var deltaY = evt.clientY - mouseY;
+	mouseX = evt.clientX;
+	mouseY = evt.clientY;
+	rotateScene(deltaX, deltaY);
+}
+
+function onMouseDown(evt) {
+	evt.preventDefault();
+	mouseDown = true;
+	mouseX = evt.clientX;
+	mouseY = evt.clientY;
+	console.log(mouseX);
+	console.log(mouseY);
+	
+}
+
+function onMouseUp(evt) {
+	evt.preventDefault();
+	mouseDown = false;
+	
+}
+
+
+function rotateScene(deltaX, deltaY) {
+    sphere.rotation.y += deltaX / 100;
+    sphere.rotation.x += deltaY / 100;
+}
+
+function updateCamera(evt) {
+	camera.position.z = clamp((camera.position.z - evt.deltaY / 100.0), 10, 25)
+	if (camera.position.z < 20) {
+		atmosphere.scale.set(1.2, 1.2, 1.2)
+	}
+	else {
+		atmosphere.scale.set(1.1, 1.1, 1.1)
+	}
+		
+}
+
+function clamp(num, min, max){
+	return Math.min(Math.max(num, min), max);
 }
 
 function animate() {
 	sphere.rotation.y += 0.001;
-	group.rotation.y  = mouse.x
 	renderer.render( scene, camera );
+	
 }
 renderer.setAnimationLoop( animate )
 
-
-
-addEventListener('click', () => {
-	mouse.x = (event.clientX / innerWidth) * 2 - 1
-	mouse.y = -(event.clientY / innerHeight) * 2 + 1
-	
-	
-	
-})
+addEventListener('mousemove', function (e) {onMouseMove(e);}, false);
+addEventListener('mousedown', function (e) {onMouseDown(e);}, false);
+addEventListener('mouseup', function (e) {onMouseUp(e);}, false);
+addEventListener('wheel', function (e) {updateCamera(e);}, false);
