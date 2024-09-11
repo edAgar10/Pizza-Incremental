689dab23c01cce437c26553479bc41360d8bf285
edAgar10 <edward10agar@gmail.com>
2024-09-11 16:46:39 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -8,10 +8,9 @@ import atmFragmentShader from './Shaders/atmFragment.glsl?raw'
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer({antialias:true});
-renderer.setSize( window.innerWidth, window.innerHeight );
+const renderer = new THREE.WebGLRenderer({antialias:true, canvas: document.querySelector('canvas')});
+renderer.setSize( window.innerWidth * 0.4, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
-document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.SphereGeometry( 5, 50, 50 );
 const material = new THREE.ShaderMaterial({vertexShader, fragmentShader, uniforms: {globeTexture: {value: new THREE.TextureLoader().load('./Assets/worldmap.jpg')}}});
