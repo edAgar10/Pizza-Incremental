90a29c096b8d01d3f93bbc9099da1cd544e63802
edAgar10 <edward10agar@gmail.com>
2024-09-22 23:36:15 +0100

    Add files via upload

--- game.js
+++ game.js
@@ -1,48 +1,104 @@
-const gen1 = document.getElementById("gen1")
+import { clamp } from "three/src/math/MathUtils.js"
 
-var totalmoney = 150
+const gen1 = document.getElementById("gen1")
+const gen2 = document.getElementById("gen2")
+const gen3 = document.getElementById("gen3")
+
+var totalmoney = 20000000
+
+let wheat = {
+	name: "wheat",
+	total: 500
+} 
+var milk = {
+	name: "milk",
+	total: 1000,
+	cap: 2000
+}
+
+
+
 var generators = []
+var buildings = []
 var lastUpdate = Date.now()
 
-let generator = {
+let wheatField = {
 	cost: Math.pow(Math.pow(50,1), 1),
-	bought: 0,
+	name: "Wheat Field",
+	amount: 1, 
+	mult: 1
+}
+
+generators.push(wheatField)
+document.getElementById("gen1Title").innerHTML = wheatField.name + "<br>";
+
+
+let cowsGen = {
+	cost: Math.pow(Math.pow(50,1), 1),
+	name: "Cows",
 	amount: 0, 
 	mult: 1
 }
 
-let generator2 = {
+generators.push(cowsGen)
+document.getElementById("gen2Title").innerHTML = cowsGen.name  + "<br>";
+
+var maxPlants = 0;
+var unnassignedPlants = 0;
+
+let vegPatch = {
 	cost: Math.pow(Math.pow(50,1), 1),
-	bought: 0,
-	amount: 0, 
-	mult: 1
+	name: "Vegetable Patch",
+	amount: 0,
+	value: maxPlants, 
+	increase: 1
 }
 
-generators.push(generator)
-generators.push(generator2)
+
+var tomatos = 16
+
+
+buildings.push(vegPatch)
+document.getElementById("gen3Title").innerHTML = vegPatch.name  + "<br>";
 
 const button1 = document.getElementById("button1")
 button1.addEventListener("click",  () => buyGenerator(1));
 const button2 = document.getElementById("button2")
 button2.addEventListener("click",  () => buyGenerator(2));
+const button3 = document.getElementById("button3")
+button2.addEventListener("click",  () => buyBuilding(1));
+
 
 function format(amount) {
 	let power = Math.floor(Math.log10(amount))
-	let mantissa = amount / Math.pow(10, power)
+	let mantissa = amount / Math.pow(1, power)
 	if (power < 3) return amount.toFixed(2)
 	return mantissa.toFixed(2) + "e" + power
 	
 }
 
+function clampResource(resource, min, max) {
+	return Math.min(Math.max(this,min), max);
+}
+
 
 function updateUI() {
 	document.getElementById("money").textContent = "Money: £" + format(totalmoney);
-	gen1.innerHTML = "<br>Amount: " + generator.amount + "<br>Cost: " + format(generator.cost);
-	gen2.innerHTML = "<br>Amount: " + generator2.amount + "<br>Cost: " + format(generator2.cost);
+	document.getElementById("wheat").textContent = "Wheat: " + format(wheat.total);
+	document.getElementById("milk").textContent = "Milk: " + (milk.total).toFixed(2) + " / " + (milk.cap).toFixed(2);
+
+	document.getElementById("availablePlants").textContent = unnassignedPlants + " / " + maxPlants
+
+
+	gen1.innerHTML = "<br>Amount: " + wheatField.amount + "<br>Cost: " + format(wheatField.cost);
+	gen2.innerHTML = "<br>Amount: " + cowsGen.amount + "<br>Cost: " + format(cowsGen.cost);
+	gen3.innerHTML = "<br>Amount: " + vegPatch.amount + "<br>Cost: " + format(vegPatch.cost);
 }
 
 function productionLoop(diff){
-	totalmoney += generator.amount * generator.mult * diff
+	wheat.total += wheatField.amount * wheatField.mult * diff
+	milk.total += cowsGen.amount * cowsGen.mult * diff
+	milk.total = clamp(milk.total, 0, milk.cap)
 	
 }
 
@@ -55,6 +111,13 @@ function buyGenerator(i) {
 	g.bought += 1
 	g.mult *= 1.05
 	g.cost *= 1.5
+}
+
+function buyBuilding(i) {
+	console.log("building bought")
+	let b = buildings[i-1]
+	if (b.cost > totalmoney) return
+	totalmoney -= b.cost
 }
 
 
